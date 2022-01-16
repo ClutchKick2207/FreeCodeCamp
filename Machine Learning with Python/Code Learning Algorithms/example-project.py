9109b19ea1dc51a291eadb0304cbb83730c9e639
Ariz Ahmed <ariz.ahmed@wellard.net.au>
2022-01-16 15:52:30 +0800

    Update example-project.py

--- Machine Learning with Python/Code Learning Algorithms/example-project.py
+++ Machine Learning with Python/Code Learning Algorithms/example-project.py
@@ -20,3 +20,10 @@ from six.moves import urllib #For potential backwards compatibility with certain
 import tensorflow.compat.v2.feature_column as fc
 
 import tensorflow as tf
+
+# Loading the dataset.
+dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv')
+dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv')
+
+y_train = dftrain.pop('survived')
+y_eval = dfeval.pop('survived')
