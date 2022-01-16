7557a529578dbe0c88cbaafab3cb950cfc46561e
Ariz Ahmed <ariz.ahmed@wellard.net.au>
2022-01-16 15:49:24 +0800

    Update example-project.py

--- Machine Learning with Python/Code Learning Algorithms/example-project.py
+++ Machine Learning with Python/Code Learning Algorithms/example-project.py
@@ -1,4 +1,9 @@
+#---README---
+
 # I decided to have a pure Python file in this folder for the example Titanic Project
 # So that you can look at the code all in one place
 
 #I'll put in a few notes in here as well.
+
+#---Start of Code---
+
